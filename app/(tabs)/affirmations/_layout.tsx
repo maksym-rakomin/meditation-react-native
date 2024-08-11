b980d93b5cb929fce36c7e25d33bae6b233ecec2
Maks Rakomin <maks.rakomin@greenice.net>
2024-08-11 06:49:28 +0200

    Add affirmation layout and practice screens
    This commit introduces a new layout for the affirmations tab and a detailed practice screen to display individual affirmations. It also relocates the existing affirmations file to act as an index for this feature.

---
+++ app/(tabs)/affirmations/_layout.tsx
@@ -0,0 +1,18 @@
+import {Stack, Tabs} from "expo-router";
+
+const AffirmationsLayout = (() => {
+  return (
+    <Stack>
+      <Stack.Screen
+        name="index"
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="[itemId]"
+        options={{headerShown: false}}
+      />
+    </Stack>
+  )
+})
+
+export default AffirmationsLayout
